52a893f8e7f720b4fc7c294b59ad62b8b752b53e
bedoyama <bedoyama@gmail.com>
2019-02-05 20:21:15 -0800

    added CORS for local dev

--- src/main/java/com/telluriac/repository/BookRepository.java
+++ src/main/java/com/telluriac/repository/BookRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.telluriac.dto.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "book", path = "book")
+@CrossOrigin(origins = "http://localhost:8000")
 public interface BookRepository extends MongoRepository<Book, String> {
 
     Book findByGuid(String guid);
